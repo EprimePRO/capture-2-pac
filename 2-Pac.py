3031b89a46bdb82d4b30f89e1d4804004b3f14a0
EprimePRO <emanuel_rios9@yahoo.com>
2019-12-06 02:38:31 -0600

    changed agents

--- 2-Pac.py
+++ 2-Pac.py
@@ -126,11 +126,9 @@ class ReflexCaptureAgent(CaptureAgent):
     """
 
         # You can profile your evaluation time by uncommenting these lines
-        start = time.time()
+        # start = time.time()
         (v, action) = self.maxValue(gameState, -2147483648, 2147483648, 0, 'Stop')
-        deltaTime = time.time() - start
-        if deltaTime>=1: return ''
-        print 'eval time for agent %d: %.4f' % (self.index, deltaTime)
+        # print 'eval time for agent %d: %.4f' % (self.index, time.time() - start)
 
         return action
 
